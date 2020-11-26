895926ee8f1aac7c3c237123dc7e894b14ab287f
Al Amin Hossain <alamin.mistrisolutions@gmail.com>
2020-11-26 15:47:36 +0600

    Vue-Tailwind

---
+++ Vue-Tailwind/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app h-screen bg-gray-200 text-center">
+    <h1 class="text-6xl text-green-300 p-20">Vue project with Tailwind CSS</h1>
+
+    <h4 class="text-6xl text-green-300 p-10">-V1.9.6</h4>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
